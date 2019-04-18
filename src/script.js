7aeb6efc077cb3de7bfa61f5647f20c4c895f474
Nemanja <ekonemanja@gmail.com>
2019-04-18 22:08:57 +0200

    add some change on footer animation

--- src/script.js
+++ src/script.js
@@ -86,7 +86,7 @@ window.addEventListener('scroll', function() {
     const bodyClientHeight = document.body.clientHeight;
     let scrollHeight = this.scrollY;
 
-    ((bodyClientHeight - $lastSectionClientHeight + 5) < scrollHeight) ? $footer.style.transform = 'translateY(-100vh)' : $footer.style.transform = 'translateY(0)';
+    ((bodyClientHeight - $lastSectionClientHeight - 1) < scrollHeight) ? $footer.style.transform = 'translateY(-100vh)' : $footer.style.transform = 'translateY(0)';
     $footer.style.transition = 'transform .4s ease';
   })
 });
